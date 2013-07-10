94460ef97363264c7dbfd74f4c59e27b9de2b911
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-09 20:34:33 -0500

    Ushare functionality

--- app/assets/javascripts/controllers/modals/unshare_job_modal_controller.js
+++ app/assets/javascripts/controllers/modals/unshare_job_modal_controller.js
@@ -1,10 +1,12 @@
 PlanSource.UnshareJobController = Ember.ObjectController.extend({
 
 	unshareJob : function(){
-		this.get("model").get("shares").each(function(share){
+		var self = this;
+		this.get("model").get("shares").forEach(function(share){
 			if(share.get("user").get("id") == user_id){
 				share.deleteRecord();
-  			share.save();
+				share.save();
+				self.get("model").deleteRecord();
 			}
 		});
 		this.send("close");
@@ -12,7 +14,7 @@ PlanSource.UnshareJobController = Ember.ObjectController.extend({
 
 	keyPress : function(e){
 		if (e.keyCode == 13)
-			this.addJob();
+			this.unshareJob();
 	}
 
 });
