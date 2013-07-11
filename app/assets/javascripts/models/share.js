d2c3f0b4ad710be45098178901c46a5c97d35348
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-11 17:53:03 -0500

    Add a delete callback to update shares

--- app/assets/javascripts/models/share.js
+++ app/assets/javascripts/models/share.js
@@ -1,4 +1,8 @@
 PlanSource.Share = DS.Model.extend({
 	user : DS.belongsTo("PlanSource.User"),
-	job : DS.belongsTo("PlanSource.Job")
+	job : DS.belongsTo("PlanSource.Job"),
+
+	didDelete : function(){
+		PlanSource.Job.find();
+	}
 });
