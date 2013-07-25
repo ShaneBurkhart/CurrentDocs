8323ae1bd35175bd983a1582a6e0d1f2c234d716
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-25 13:40:51 -0500

    Added job_name to serializer

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -1,6 +1,10 @@
 class PlanSerializer < ActiveModel::Serializer
   attributes :id, :plan_name, :plan_num, :filename, :updated_at,
-  		:plan_file_name, :plan
+  		:plan_file_name, :plan, :job_name
   has_one :job
   embed :ids, include: false
+
+  def job_name
+    object.job.name
+  end
 end
