046af92991e725594b7b746d8283269d57456597
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-25 14:05:03 -0500

    Added updated_at to serializer plans

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -1,6 +1,6 @@
 class PlanSerializer < ActiveModel::Serializer
   attributes :id, :plan_name, :plan_num, :filename, :updated_at,
-  		:plan_file_name, :plan, :job_name
+  		:plan_file_name, :plan, :job_name, :updated_at
   has_one :job
   embed :ids, include: false
 
