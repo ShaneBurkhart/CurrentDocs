cd1241373fb1e183637d9eadfd825cea6d885a65
Ryan Endacott <rzeg24@gmail.com>
2013-08-25 17:09:39 -0500

    no longer using paperclip to_read

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -48,8 +48,7 @@ class Plan < ActiveRecord::Base
   def page_sizes
     sizes = []
     return sizes unless self.plan.file?
-    puts "The urlllllllllllllllllllllll is: #{self.plan.url}"
-    reader = PDF::Reader.new(plan.to_file.read)
+    reader = PDF::Reader.new(open(self.plan.url))
     reader.pages.each do |page|
       attrs = page.attributes[:MediaBox]
       width = attrs[2].to_f / 72.0
