b58b716daa624427941700155f5ff77376d857e2
ShaneBurkhart <shaneburkhart@gmail.com>
2013-09-03 13:44:27 -0500

    Redirect to app if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_filter :go_to_app?
+
   def index
     @user = User.new
   end
+
+  private
+
+    def go_to_app?
+      redirect_to app_path if current_user
+    end
 end
