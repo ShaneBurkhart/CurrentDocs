a3505ebb47ab727f5110683b1fcd2a5e2b06d0ce
ShaneBurkhart <shaneburkhart@gmail.com>
2013-09-10 15:20:35 -0500

    Correct routing of page flow for adding and cancelling subscription

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -7,14 +7,19 @@ class AccountController < ApplicationController
 
   def update
     u = user
-    u.type = params[:user][:type]
-    if u.save
-      if u.type == "Viewer"
-        flash[:notice] = "You are now a Viewer!"
+    if params[:user][:type] == "Viewer"
+      u.type = params[:user][:type]
+      if u.save
+        if u.type == "Viewer"
+          flash[:notice] = "You are now a Viewer!"
+        else
+          flash[:notice] = "You are now a Manager! Thank you for subscribing!"
+        end
+        redirect_to app_path
       else
-        flash[:notice] = "You are now a Manager! Thank you for subscribing!"
+        flash[:error] = "Not a valid account type"
+        render "select"
       end
-      redirect_to app_path
     else
       flash[:error] = "Not a valid account type"
       render "select"
@@ -26,6 +31,35 @@ class AccountController < ApplicationController
   end
 
   def update_billing
+    if params["stripeToken"].blank?
+      flash[:error] = "You must enter billing information."
+      render "billing"
+      return
+    end
+    customer = Stripe::Customer.create(
+      :email => user.email,
+      :card  => params[:stripeToken]
+    )
+
+    if customer
+      user.stripe_customer_id = customer.id
+      user.type = "Manager"
+      if user.save
+        flash[:notice] = "You are now a Manager! Thank you for subscribing!"
+        redirect_to app_path
+      else
+        flash[:error] = "There was an error with your card.  Please make sure you entered it correctly"
+        render "billing"
+      end
+    else
+      flash[:error] = "There was an error with your card.  Please make sure you entered it correctly"
+      render "billing"
+    end
 
   end
+
+    rescue_from Stripe::CardError do |e|
+      flash[:error] = e.message
+      render "billing"
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130829162749) do
+ActiveRecord::Schema.define(:version => 20130910200405) do
 
   create_table "jobs", :force => true do |t|
     t.string   "name"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(:version => 20130829162749) do
     t.string   "invited_by_type"
     t.string   "type"
     t.string   "authentication_token"
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
