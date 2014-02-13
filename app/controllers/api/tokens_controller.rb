b038b75a99399187f5c371a3e924f2629aadf9ab
Shane Burkhart <shaneburkhart@gmail.com>
2014-02-12 21:46:40 -0600

    Added authentication token

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -1,16 +1,24 @@
 class Api::TokensController < ApplicationController
 
-	def create
-		@user = User.find_by_email params[:email]
-		if @user.nil?
-			render json: {message: "Invalid email or password"}
-		else
-			if @user.valid_password? params[:password]
-				render json: {token: @user.authentication_token}
-			else
-				render json: {message: "Invalid email or password"}
-			end
-		end
-	end
+  def create
+    @user = User.find_by_email params[:email]
+    if @user.nil?
+      render json: {message: "Invalid email or password"}
+    else
+      if @user.valid_password? params[:password]
+          if !@user.
+        render json: {token: @user.authentication_token}
+      else
+        render json: {message: "Invalid email or password"}
+      end
+    end
+  end
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless self.class.unscoped.where(authentication_token: token).first
+    end
+  end
 
 end
