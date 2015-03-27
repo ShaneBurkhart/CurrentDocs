6eef865a0d1dbbe55d7bac3b4afe01f50c67b1c0
Shane Burkhart <shaneburkhart@gmail.com>
2015-03-27 23:11:41 +0000

    Added plan_update_at to json output.

--- app/serializers/plan_serializer.rb
+++ app/serializers/plan_serializer.rb
@@ -1,6 +1,7 @@
 class PlanSerializer < ActiveModel::Serializer
   attributes :id, :plan_name, :plan_num, :filename, :updated_at,
-  		:plan_file_name, :plan, :job_name, :updated_at
+  		:plan_file_name, :plan, :job_name, :updated_at,
+		:plan_updated_at
   has_one :job
   embed :ids, include: false
 
