51658bc13a1334e412725fb39ea7f2ea397e39c2
Shane Burkhart <shaneburkhart@gmail.com>
2015-04-12 22:30:37 +0000

    Added name to email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
     include SendGrid
-    default from: "plansource-noreply@plansource.io"
+    default from: "PlanSource <plansource-noreply@plansource.io>"
 
     def share_notification(share)
 	@share = share
