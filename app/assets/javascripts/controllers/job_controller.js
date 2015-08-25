c5e8497cf878e8c157965692ad5246fa80d341ac
Shane Burkhart <shaneburkhart@gmail.com>
2015-08-25 13:19:24 -0500

    Added controls to archive and unarchive jobs.

--- app/assets/javascripts/controllers/job_controller.js
+++ app/assets/javascripts/controllers/job_controller.js
@@ -1,4 +1,13 @@
 PlanSource.JobController = Ember.ObjectController.extend({
+  archiveJob: function() {
+    this.get('model').set('archived', true);
+    this.get('model').save();
+  },
+
+  unarchiveJob: function() {
+    this.get('model').set('archived', false);
+    this.get('model').save();
+  },
 
 	back : function(){
 		window.history.go(-1);
