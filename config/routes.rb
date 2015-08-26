8a255cb08f5d7b6e2db6ed597f0b6ea613f04ac1
Shane Burkhart <shaneburkhart@gmail.com>
2015-08-26 14:25:46 -0500

    Added index, delete, and images endpoints for ve slider images.

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,35 @@
 PlanSource::Application.routes.draw do
   get "signup_links/edit"
 
-    authenticated :user do
-	root :to => 'home#index'
-    end
-    root :to => "home#index"
-    devise_for :users
+  authenticated :user do
+    root :to => 'home#index'
+  end
 
-    namespace :api do
-	resources :jobs, except: ["new", "edit"]
-	resources :plans, except: ["new", "edit", "index"]
-	get '/plans/embedded/:id' => 'plans#show_embedded'
-	get '/user/contacts' => 'users#contacts'
-	post '/user/contacts' => 'users#add_contacts'
-	resources :shares, only: ["create", "destroy", "update"]
-	post "/shares/batch" => "shares#batch"
-	resource :upload, only: ["create"]
-	resource :token, only: ["create"] #only retrieve token
-	match "/download/:id" => "downloads#download"
-	post '/message' => 'messages#group'
-    end
+  root :to => "home#index"
+  devise_for :users
 
-    resources :users, only: ["index", "edit", "update", "destroy"]
-    get "/users/:id/demote" => "users#demote", as: "demote"
-    get "/users/signup_link/:key", to: "signup_links#edit", as: "signup_link"
-    put "/users/signup_link/:key", to: "signup_links#update"
+  namespace :api do
+    resources :jobs, except: ["new", "edit"]
+    resources :plans, except: ["new", "edit", "index"]
+    get '/plans/embedded/:id' => 'plans#show_embedded'
+    get '/user/contacts' => 'users#contacts'
+    post '/user/contacts' => 'users#add_contacts'
+    resources :shares, only: ["create", "destroy", "update"]
+    post "/shares/batch" => "shares#batch"
+    resource :upload, only: ["create"]
+    resource :token, only: ["create"] #only retrieve token
+    match "/download/:id" => "downloads#download"
+    post '/message' => 'messages#group'
+  end
 
-    match "/mobile" => "mobile#index"
-    match "/app" => "app#index"
+  resources :users, only: ["index", "edit", "update", "destroy"]
+  get "/users/:id/demote" => "users#demote", as: "demote"
+  get "/users/signup_link/:key", to: "signup_links#edit", as: "signup_link"
+  put "/users/signup_link/:key", to: "signup_links#update"
 
+  match "/mobile" => "mobile#index"
+  match "/app" => "app#index"
+  match "/ve_pictures" => "ve_pictures#index"
+  match "/ve_pictures/all" => "ve_pictures#images"
+  post "/ve_pictures/delete" => "ve_pictures#destroy"
 end
