71e5956f08d77858d07e9e5c757f1593e5310806
Shane Burkhart <shaneburkhart@gmail.com>
2015-09-22 14:41:28 -0500

    Added tab switching in ember.

--- app/assets/javascripts/controllers/job_controller.js
+++ app/assets/javascripts/controllers/job_controller.js
@@ -1,4 +1,26 @@
 PlanSource.JobController = Ember.ObjectController.extend({
+  tab: 'Plans',
+
+  changeTab: function(tab) {
+    this.set('tab', tab);
+  },
+
+  isPlansTab: function() {
+    return this.get('tab') === 'Plans';
+  }.property('tab'),
+
+  isShopsTab: function() {
+    return this.get('tab') === 'Shops';
+  }.property('tab'),
+
+  isConsultantsTab: function() {
+    return this.get('tab') === 'Consultants';
+  }.property('tab'),
+
+  isCalcTab: function() {
+    return this.get('tab') === 'Calcs & Misc';
+  }.property('tab'),
+
   archiveJob: function() {
     this.get('model').set('archived', true);
     this.get('model').save();
