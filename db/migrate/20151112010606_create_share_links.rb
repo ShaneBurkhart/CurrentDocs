549a6b832ff9652b994abacddf3b0192fc14e124
Shane Burkhart <shaneburkhart@gmail.com>
2015-11-11 19:30:56 -0600

    Added job_id and email_shared_with to share_link migration.

--- db/migrate/20151112010606_create_share_links.rb
+++ db/migrate/20151112010606_create_share_links.rb
@@ -2,7 +2,9 @@ class CreateShareLinks < ActiveRecord::Migration
   def change
     create_table :share_links do |t|
     	t.string :token
+    	t.integer :job_id
     	t.integer :user_id
+    	t.string :email_shared_with
     	t.integer :company_name
       t.timestamps
     end
