fa1689ef127f4444d6b9eb132e4cab062fdfa277
Drew Wetherington <dgwetherington@gmail.com>
2016-05-25 02:03:12 -0500

    before I change mailer .persisted?

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -2,10 +2,6 @@ class PdfController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    url = 'http://localhost:3000/system/plans/plans/000/000/251/original/Argonaut.pdf'
-    pdf_params = { id: url }.to_param
-    @pdf_url = url
-    # puts pdf_url
     render :layout => false
   end
 
