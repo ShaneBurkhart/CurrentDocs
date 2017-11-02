288acb22cada8d62f51e25d0df9cf0bafda07eb9
Shane Burkhart <shaneburkhart@gmail.com>
2017-11-02 09:54:43 -0500

    Refactor photos route.

--- app/assets/javascripts/plan_source/routes/job_photos_route.js
+++ app/assets/javascripts/plan_source/routes/job_photos_route.js
@@ -1,40 +1,18 @@
 PlanSource.JobPhotosRoute = Ember.Route.extend({
   tab: 'Photos',
 
-  setupController: function (controller, model) {
+  setupController: function (controller) {
     var jobController = this.controllerFor('job');
-
-    controller.set('jobController', jobController);
-    controller.set('content', model);
-  },
-
-  model: function () {
-    var jobPromise = this.modelFor('job');
+    var job = jobController.get('model');
     var resolvedPhotos = Em.A();
 
-    // Check if the model is a promise
-    if (jobPromise.then) {
-      // jobPromise is a promise
-      return Em.Deferred.promise(function (p) {
-        jobPromise.then(function (job) {
-          job.getPhotos(function (photos) {
-            for (var i = 0; i < photos.length; i++) {
-              resolvedPhotos.push(photos[i]);
-            }
+    job.getPhotos(function (photos) {
+      for (var i = 0; i < photos.length; i++) {
+        resolvedPhotos.push(photos[i]);
+      }
+    });
 
-            p.resolve(photos);
-          });
-        });
-      });
-    } else {
-      // jobPromise is a Job object
-      jobPromise.getPhotos(function (photos) {
-        for (var i = 0; i < photos.length; i++) {
-          resolvedPhotos.push(photos[i]);
-        }
-      });
-    }
-
-    return resolvedPhotos;
+    controller.set('jobController', jobController);
+    controller.set('content', resolvedPhotos);
   }
 });
