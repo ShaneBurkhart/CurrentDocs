09ff89c77cfbc3ca98c6607b7b678238fd37799b
Shane Burkhart <shaneburkhart@gmail.com>
2017-11-09 13:35:47 -0600

    Fix asi migration to include attachments.

--- db/migrate/20171109191704_move_asis.rb
+++ db/migrate/20171109191704_move_asis.rb
@@ -7,12 +7,20 @@ class MoveASIs < ActiveRecord::Migration
       plans = Plan.where(job_id: job_id, tab: "ASI").order("id ASC")
 
       plans.each do |plan|
-        ASI.create(
+        asi = ASI.create(
           job_id: job_id,
           user_id: plan.job.user_id,
           status: "Open",
           subject: plan.plan_name,
           notes: "Imported from 'ASI' tab."
+        )
+
+        break if !asi
+
+        ASIAttachment.create(
+          asi_id: asi.id,
+          filename: plan.plan_file_name,
+          s3_path: plan.plan.path
         )
       end
     end
