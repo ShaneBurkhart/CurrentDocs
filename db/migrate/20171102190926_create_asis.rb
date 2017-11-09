585c29d0639bc952533475881eddbe85e588f84f
Shane Burkhart <shaneburkhart@gmail.com>
2017-11-09 13:17:41 -0600

    ASI transfer migration to separate migration.

--- db/migrate/20171102190926_create_asis.rb
+++ db/migrate/20171102190926_create_asis.rb
@@ -14,22 +14,5 @@ class CreateASIs < ActiveRecord::Migration
 
       t.timestamps
     end
-
-    # Get all job ids and go through one by one to not overdo memory
-    job_ids = Job.pluck(:id)
-
-    job_ids.each do |job_id|
-      plans = Plan.where(job_id: job_id, tab: "ASI").order("id ASC")
-
-      plans.each do |plan|
-        ASI.create(
-          job_id: job_id,
-          user_id: plan.job.user_id,
-          status: "Open",
-          subject: plan.plan_name,
-          notes: "Imported from 'ASI' tab."
-        )
-      end
-    end
   end
 end
