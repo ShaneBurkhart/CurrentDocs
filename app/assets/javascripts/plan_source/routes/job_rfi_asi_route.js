e5f4b163ed3e2cd915c503e96469722de2110427
Shane Burkhart <shaneburkhart@gmail.com>
2017-11-14 20:33:34 -0600

    Change default filter for rfi and asi route to 'all'

--- app/assets/javascripts/plan_source/routes/job_rfi_asi_route.js
+++ app/assets/javascripts/plan_source/routes/job_rfi_asi_route.js
@@ -4,7 +4,7 @@ PlanSource.JobRfiAsiRoute = Ember.Route.extend({
   setupController: function (controller) {
     var jobController = this.controllerFor('job');
     var job = jobController.get('model');
-    var currentFilter = controller.get('currentFilter') || 'open';
+    var currentFilter = controller.get('currentFilter') || 'all';
 
     jobController.set('currentTab', this.tab);
     controller.set('jobController', jobController);
