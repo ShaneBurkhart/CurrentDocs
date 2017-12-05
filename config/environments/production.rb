cf11e562524ba425a90daf9b4034485ea477a611
Shane Burkhart <shaneburkhart@gmail.com>
2017-12-04 20:14:20 -0600

    Remove sendgrid settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,15 @@ PlanSource::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'plansource.io' }
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  #config.action_mailer.smtp_settings = {
+    #:address        => 'smtp.sendgrid.net',
+    #:port           => '587',
+    #:authentication => :plain,
+    #:user_name      => ENV['SENDGRID_USERNAME'],
+    #:password       => ENV['SENDGRID_PASSWORD'],
+    #:domain         => 'heroku.com',
+    #:enable_starttls_auto => true
+  #}
 
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
